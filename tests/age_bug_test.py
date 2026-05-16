fec9f176737c4b3738f7c97d34afa515ae05b2e8
lnguyen109-lab <lnguyen109@g.ucla.edu>
2026-05-16 16:29:43 -0700

    Update age_bug_test.py

--- tests/age_bug_test.py
+++ tests/age_bug_test.py
@@ -7,7 +7,12 @@ sys.path.insert(0, str(Path(__file__).parent.parent))
 
 from src import UserProfile
 
+'''
+get_age() function should return these numbers correctly, and be able to tell when someone's birthday has passed this year
+'''
+
 class TestUserProfile:
+    # Some of these should return the wrong age
     def test_correct_age(self):
       assert UserProfile.get_age("2/28/2000") == 26
       assert UserProfile.get_age("3/28/2000") == 26
