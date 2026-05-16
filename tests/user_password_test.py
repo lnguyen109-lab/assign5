3d7a8016651fdf622446a7821954ea188dabe973
lnguyen109-lab <lnguyen109@g.ucla.edu>
2026-05-16 16:26:35 -0700

    Updated user_password_test.py

--- tests/user_password_test.py
+++ tests/user_password_test.py
@@ -7,18 +7,25 @@ sys.path.insert(0, str(Path(__file__).parent.parent))
 
 from src import UserProfile
 
-class TestUserPassword:
-    
-    def test_valid_password(self):
-        assert UserProfile.valid_password("") == True
-        assert UserProfile.valid_password("") == True
-        assert UserProfile.valid_password("") == True
-        assert UserProfile.valid_password("") == True
+'''
+Must be at least 8 characters long
+Must contain one uppercase, one lowercase, one digit and one special character from: @$!%*?&
+'''
 
-        assert UserProfile.valid_password("") == False
-        assert UserProfile.valid_password("") == False
-        assert UserProfile.valid_password("") == False
-        assert UserProfile.valid_password("") == False
+class TestUserPassword:
+    # Some of the true statements should fail
+    def test_valid_password(self):
+        assert UserProfile.valid_password("passwordTest109!") == True
+        assert UserProfile.valid_password("@ThisPasses3") == True
+        assert UserProfile.valid_password("Testing123!") == True
+        assert UserProfile.valid_password("&Can15MinutesSaveYou15%OnCarInsurance?") == True
+        assert UserProfile.valid_password("1234567890Yeah!") == True
+        assert UserProfile.valid_password("helloWorld67!") == True
+
+        assert UserProfile.valid_password("ThereIsNothingSpecialHere") == False
+        assert UserProfile.valid_password("nevermore!") == False
+        assert UserProfile.valid_password("0987654321") == False
+        assert UserProfile.valid_password("WHATDOYOUWANT?") == False
 
   if __name__ == "__main__":
     pytest.main([__file__, "-v"])
