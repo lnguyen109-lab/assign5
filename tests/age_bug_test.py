3a2106477d690e5fc6725f157f4450ff772fa166
lnguyen109-lab <lnguyen109@g.ucla.edu>
2026-05-16 18:47:46 -0700

    Update age_bug_test.py

--- tests/age_bug_test.py
+++ tests/age_bug_test.py
@@ -14,9 +14,27 @@ get_age() function should return these numbers correctly, and be able to tell wh
 class TestUserProfile:
     # Some of these should return the wrong age
     def test_correct_age(self):
-      assert UserProfile.get_age(2000, 2, 28) == 26
-      assert UserProfile.get_age(2000, 3, 28) == 26
-      assert UserProfile.get_age(2000, 4, 28) == 26
-      assert UserProfile.get_age(2000, 5, 28) == 25
-      assert UserProfile.get_age(2000, 6, 28) == 25
-      assert UserProfile.get_age(2000, 7, 28) == 25
+
+        reference_date1 = datetime(2000, 2, 28)
+        assert UserProfile.get_age(reference_date1) == 26
+
+        reference_date2 = datetime(2000, 3, 28)
+        assert UserProfile.get_age(reference_date2) == 26
+        
+        reference_date3 = datetime(2000, 4, 28)
+        assert UserProfile.get_age(reference_date3) == 26
+        
+        reference_date4 = datetime(2000, 5, 28)
+        assert UserProfile.get_age(reference_date4) == 25
+        
+        reference_date5 = datetime(2000, 6, 28)
+        assert UserProfile.get_age(reference_date5) == 25
+
+        reference_date6 = datetime(2000, 7, 28)
+        assert UserProfile.get_age(reference_date6) == 25
+
+
+
+
+
+
